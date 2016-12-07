90c60a95f3972af4fd5532e218f38b1c600550a5
unknown <andy400@web.de>
2016-12-07 18:48:23 +0100

    init commit

---
+++ desktop/src/systems/crigges/informaticup/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package systems.crigges.informaticup.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import systems.crigges.informaticup.MainFrame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainFrame(), config);
+	}
+}
