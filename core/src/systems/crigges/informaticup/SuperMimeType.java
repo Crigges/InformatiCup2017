3aa1a14575504239332461b3fb37f9d187b318a6
Crigges Wurst <andy400@web.de>
2017-01-09 06:39:18 +0100

    Removed unused classes | improoved classnames | added package stucture | started working on java doc

--- core/src/systems/crigges/informaticup/SuperMimeType.java
+++
@@ -1,8 +0,0 @@
-package systems.crigges.informaticup;
-
-public enum SuperMimeType {
-
-	Rar, Zip, PDF, Word, PowerPoint, Text, Binary, Image, Folder;
-
-}
-
--- desktop/src/systems/crigges/informaticup/desktop/DesktopLauncher.java
+++ desktop/src/systems/crigges/informaticup/desktop/DesktopLauncher.java
@@ -2,7 +2,8 @@ package systems.crigges.informaticup.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import systems.crigges.informaticup.MainFrame;
+
+import systems.crigges.informaticup.general.MainFrame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
