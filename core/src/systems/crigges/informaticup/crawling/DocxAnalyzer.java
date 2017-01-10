afd52697f895452c868e667b5670bab91f530186
Crigges Wurst <andy400@web.de>
2017-01-10 02:43:15 +0100

    Added javadoc for DocxAnalyzer.java

--- core/src/systems/crigges/informaticup/crawling/DocxAnalyzer.java
+++ core/src/systems/crigges/informaticup/crawling/DocxAnalyzer.java
@@ -10,31 +10,51 @@ import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFPictureData;
 
+/**
+ * This class allows simple text and image extraction out of Mircosoft's Office
+ * .docx files. Files need to be passed virtual as array of bytes for faster
+ * processing.
+ * 
+ * @author Rami Aly & Crigges
+ */
 public class DocxAnalyzer {
 
 	private XWPFDocument doc;
 
+	/**
+	 * Creates a new DocxAnalyzer out of the given file.
+	 * 
+	 * @param data
+	 *            the .docx file represented as byte array
+	 * @throws IOException
+	 *             if file can't be read or is protected
+	 */
 	public DocxAnalyzer(byte[] data) throws IOException {
 		doc = new XWPFDocument(new ByteArrayInputStream(data));
 	}
 
+	/**
+	 * Extracts the raw unformatted text out of the document.
+	 * 
+	 * @return the raw text as String
+	 * @throws Exception
+	 *             if file can't be read or is protected
+	 */
 	public String getRawText() throws Exception {
 		XWPFWordExtractor ex = new XWPFWordExtractor(doc);
 		String text = ex.getText();
 		return text;
 	}
 
+	/**
+	 * Extracts all images out of the document.
+	 * 
+	 * @return the List of images contained inside the document
+	 * @throws IOException
+	 *             if file can't be read or is protected
+	 */
 	public List<XWPFPictureData> getImages() throws IOException {
 		return doc.getAllPictures();
-	}
-
-	public static void main(String[] args) throws Exception {
-		DocxAnalyzer doc = new DocxAnalyzer(Files.readAllBytes(new File("./assets/testDoc.docx").toPath()));
-		String s = doc.getRawText();
-		PrintWriter writer = new PrintWriter(new File("./test/docextract.txt"));
-		writer.print(s);
-		writer.close();
-		doc.getImages();
 	}
 
 }
