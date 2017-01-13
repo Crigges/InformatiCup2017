b7d54502f10bb94961ab123f0dc02ceaa3f8d3c0
Crigges Wurst <andy400@web.de>
2017-01-13 02:44:45 +0100

    Added javadoc for OutputFileWriter.java

--- core/src/systems/crigges/informaticup/io/OutputFileWriter.java
+++ core/src/systems/crigges/informaticup/io/OutputFileWriter.java
@@ -7,19 +7,47 @@ import java.io.UnsupportedEncodingException;
 
 import systems.crigges.informaticup.general.RepositoryDescriptor;
 
+/**
+ * This class allows to write multiple {@link RepositoryDescriptor}s to a given
+ * File.
+ * 
+ * @author Rami Aly & Andre Schurat
+ * @see RepositoryDescriptor
+ */
 public class OutputFileWriter {
 
 	private PrintWriter writer = null;
-	
-	public OutputFileWriter(File writePath) throws FileNotFoundException, UnsupportedEncodingException{
+
+	/**
+	 * Attaches a new OutputFileWriter to the given file. Existing file are
+	 * overridden.
+	 * 
+	 * @param writePath
+	 *            the target file
+	 * @throws FileNotFoundException
+	 *             if file can't be accessed
+	 * @throws UnsupportedEncodingException
+	 *             if the running JVM does not support UTF-8
+	 */
+	public OutputFileWriter(File writePath) throws FileNotFoundException, UnsupportedEncodingException {
 		this.writer = new PrintWriter(writePath, "UTF-8");
 	}
-	
-	public void write(RepositoryDescriptor repo){
-		 writer.println(repo.getName() + " " + repo.getTyp());
+
+	/**
+	 * Writes the given repository to the file.
+	 * 
+	 * @param repo
+	 *            to be written
+	 */
+	public void write(RepositoryDescriptor repo) {
+		writer.println(repo.getName() + " " + repo.getTyp());
 	}
-	
-	public void close(){
+
+	/**
+	 * Closes the writer and forces all submitted repositories to be written to
+	 * the file.
+	 */
+	public void close() {
 		writer.flush();
 		writer.close();
 	}
