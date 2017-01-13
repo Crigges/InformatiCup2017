a1e5a00f9ffff3f4dd0f897f0b7f5771981823fc
Crigges Wurst <andy400@web.de>
2017-01-13 02:38:12 +0100

    Added javadoc for SerizalzeHelper.java

--- core/src/systems/crigges/informaticup/io/SerializeHelper.java
+++ core/src/systems/crigges/informaticup/io/SerializeHelper.java
@@ -8,8 +8,21 @@ import java.io.IOException;
 import org.nustaq.serialization.FSTObjectInput;
 import org.nustaq.serialization.FSTObjectOutput;
 
+/**
+ * Simple wrapper class for FST to support fast (de-)serialization
+ * 
+ * @author Rami Aly & Andre Schurat
+ */
 public class SerializeHelper {
 
+	/**
+	 * Serializes the given object into the target file using FST. Existing
+	 * files get overridden.
+	 * 
+	 * @param f the target {@link File}
+	 * @param input the object to be serialized
+	 * @throws IOException if any IO error occurs
+	 */
 	public static void serialize(File f, Object input) throws IOException {
 		f.createNewFile();
 		FSTObjectOutput out = new FSTObjectOutput(new FileOutputStream(f));
@@ -17,6 +30,12 @@ public class SerializeHelper {
 		out.close();
 	}
 
+	/**
+	 * Deserializes the given File into a Object of the given type.
+	 * 
+	 * @param f the target {@link File}
+	 * @throws IOException if any IO error occurs
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> T deserialize(File f) throws FileNotFoundException, IOException, ClassNotFoundException {
 		T words = null;
