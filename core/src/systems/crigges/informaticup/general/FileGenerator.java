7d410bda96b61a770e0fd30ce0b50d51808186ab
Raldir <rami-aly@hotmail.de>
2017-01-14 14:44:08 +0100

    added missing WEB Repositorys UPS

--- core/src/systems/crigges/informaticup/general/FileGenerator.java
+++ core/src/systems/crigges/informaticup/general/FileGenerator.java
@@ -4,13 +4,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import systems.crigges.informaticup.crawling.RepositoryCrawler;
 import systems.crigges.informaticup.io.InputFileReader;
 import systems.crigges.informaticup.io.OutputFileWriter;
+import systems.crigges.informaticup.io.RepoCacher;
 
 public class FileGenerator {
 
 	public static void main(String[] args) throws Exception {
-		genTraining();
+		cacheRepositories();
+//		genTraining();
 //		InputFileReader allR = new InputFileReader(new File("./assets/Repositorys.txt"));
 //		List<RepositoryDescriptor> res = new ArrayList<>();
 //		for (RepositoryDescriptor des : allR.getRepositorysAndTypes()) {
@@ -40,6 +43,15 @@ public class FileGenerator {
 //			writer.write(des);
 //		}
 //		writer.close();
+	}
+	
+	private static void cacheRepositories() throws Exception{
+		RepoCacher.initThreadPool(4);
+		InputFileReader allR = new InputFileReader(new File("./assets/Repositorys.txt"));
+		for (RepositoryDescriptor des : allR.getRepositorysAndTypes()) {
+			RepoCacher.getThreaded(des.getName(), (RepositoryCrawler craw) -> {});
+		}
+		RepoCacher.shutdownThreadPool();
 	}
 	
 	private static void genTraining() throws Exception{
